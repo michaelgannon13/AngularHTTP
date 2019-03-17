02226d3b5dd16739289f0bb6e65353b4b9d3035d
Michael gannon <michael.gannon13@gmail.com>
2019-03-17 16:50:06 +0000

    basic http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  constructor(private httpClient: HttpClient){};
+  
   title = 'AngularHttpClient';
+  baseUrl = 'http://localhost:3000';
+  data = []
+
+  //http://localhost:3000/
+  getAnything(){
+    this.httpClient
+      .get(this.baseUrl + '/posts')
+      .subscribe((res : any[])=>{
+        this.data = res;
+        console.log('data array is', this.data);
+      })
+  }
 }
