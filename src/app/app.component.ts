a8f10438ecf5b50cd6db03180d85f088c1e7c367
Michael gannon <michael.gannon13@gmail.com>
2019-03-18 13:45:41 +0000

    added service calls to functions in the app component. Linked to buttons and surfaced on UI. Updates README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,30 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   private posts = [];
   private comments = [];
-  private profile = [];
+  private profiles = [];
 
   // need clarity on what this is for
   private dataObservable: Observable<any[]>;
 
   // not sure why this goes inside constructor
-  constructor(private dataService: DataService){
-  
+  constructor(private dataService: DataService){}
+
+  public getPosts(){
     this.dataService.getPosts().subscribe((res: any[])=>{
       this.posts = res;
     });
+  }
 
+  public getComments(){
     this.dataService.getComments().subscribe((res: any[])=>{
       this.comments = res;
     });
+  }
 
+  // this doesn't work since the observable is nested differently to the above observables
+  public getProfile(){
     this.dataService.getProfile().subscribe((res: any[])=>{
-      this.profile = res;
+      this.profiles = res;
     });
-
   }
 }
