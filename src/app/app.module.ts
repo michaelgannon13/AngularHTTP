8f2cb5bbe4e83e66efe7d6550ef41573aa3a31ee
Michael <m.j.gannon@accenture.com>
2019-03-24 18:13:14 +0000

    removed unused module and added list divider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { AlbumsComponent } from './components/albums/albums.component';
 import { DataComponent } from './components/data/data.component';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 
 @NgModule({
