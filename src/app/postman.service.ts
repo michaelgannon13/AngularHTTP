53f9c67e48130483fe7993317bc1f0c7a92596bb
Michael gannon <michael.gannon13@gmail.com>
2019-03-26 19:29:02 +0000

    added dummy post request

--- src/app/postman.service.ts
+++ src/app/postman.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Headers} from 'angular2/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +9,6 @@ export class PostmanService {
   constructor(private http: HttpClient) { }
   
   baseUrl = 'https://jsonplaceholder.typicode.com';
-  headers = new Headers();
-
-
 
   postSomething(data) {
     this.http.post('http://jsonplaceholder.typicode.com/posts', {
@@ -26,27 +22,6 @@ export class PostmanService {
       },
       err => {
         console.log('Error occured');
-      }
-  
-  };
-
-  // sendToAPI(form: NgForm) {
-  //   const formObj = form.value;
-  //   const serializedForm = JSON.stringify(formObj);
-  //   const req =
-  //   this.http.post('http://jsonplaceholder.typicode.com/posts', {
-  //     title: 'foo',
-  //     body: serializedForm,
-  //     userId: 1
-  //   })
-  //   .subscribe(
-  //     res => {
-  //       console.log(res);
-  //     },
-  //     err => {
-  //       console.log('Error occured');
-  //     }
-  //   );
-  // }
-
-};
+      });
+  }
+}
