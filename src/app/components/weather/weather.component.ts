28e462e35ca962d0e98cb2f0d4a890115841fd19
Michael gannon <michael.gannon13@gmail.com>
2019-04-07 20:39:15 +0100

    added images and swtich statement

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -30,10 +30,20 @@ export class WeatherComponent implements OnInit {
         this.weatherService.getLocation(this.lat, this.long)
         .subscribe((res: any[]) => {
           this.currentWeather = res;
+          console.log("Weather response", this.currentWeather)
+
           this.isWeather = true;
-            if (this.currentWeather.weather[0].main === 'Drizzle') {
-              this.backgroundImage = 'rain.jpg'
-            }
+
+          const weather = this.currentWeather.weather[0].main;
+
+          switch (weather) {
+              case 'Drizzle':
+                this.backgroundImage = 'rain.jpg';
+                break;
+              case 'Clouds':
+                this.backgroundImage = 'clouds.jpg';
+              break;
+          }
           })
       });
     } else {
